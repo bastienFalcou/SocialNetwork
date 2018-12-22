05c64ecd62b80e7a9f81c8a33583de2e4a5344b3
Bastien Falcou <bastien@fueled.com>
2018-12-22 11:22:27 +0100

    fix(SignInFragment): fix constraints issue for form to be displayed

--- app/src/main/java/com/temporaryname/bastienfalcou/testnavigation/SignInFragment.kt
+++ app/src/main/java/com/temporaryname/bastienfalcou/testnavigation/SignInFragment.kt
@@ -6,7 +6,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.findNavController
-import kotlinx.android.synthetic.main.fragment_home_screen.*
 import kotlinx.android.synthetic.main.fragment_sign_in.*
 
 class SignInFragment: Fragment() {
