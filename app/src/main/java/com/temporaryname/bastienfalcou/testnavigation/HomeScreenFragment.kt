539cd62cb1a4e583197402094ed7278f834a9d80
Bastien Falcou <bastien@fueled.com>
2018-12-22 02:00:07 +0100

    chore(Project): push home screen and handle passing the value from one to another

--- app/src/main/java/com/temporaryname/bastienfalcou/testnavigation/HomeScreenFragment.kt
+++ app/src/main/java/com/temporaryname/bastienfalcou/testnavigation/HomeScreenFragment.kt
@@ -1,31 +1,21 @@
 package com.temporaryname.bastienfalcou.testnavigation
 
-
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_home_screen.*
 
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- *
- */
-class HomeScreenFragment : Fragment() {
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+class HomeScreenFragment: Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home_screen, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        nameTextView.setText(arguments?.getString("nameArgument"))
+    }
 }
--- app/src/main/java/com/temporaryname/bastienfalcou/testnavigation/SignInFragment.kt
+++ app/src/main/java/com/temporaryname/bastienfalcou/testnavigation/SignInFragment.kt
@@ -5,6 +5,8 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
+import kotlinx.android.synthetic.main.fragment_sign_in.*
 
 class SignInFragment: Fragment() {
     override fun onCreateView(inflater: LayoutInflater,
@@ -12,5 +14,15 @@ class SignInFragment: Fragment() {
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_sign_in, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        nextButton.setOnClickListener {
+            var nameBundle = Bundle()
+            nameBundle.putString("nameArgument", theNameTextView.text.toString())
+            it.findNavController().navigate(R.id.toHomeScreenFragment, nameBundle)
+        }
     }
 }
