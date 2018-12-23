aea593324d808becf91f22763ea7543aa1b4e72d
Bastien Falcou <bastien@fueled.com>
2018-12-23 23:18:49 +0100

    refactor(APIClient): improve code quality

--- app/src/main/java/com/temporaryname/bastienfalcou/testnavigation/APIClient.kt
+++ app/src/main/java/com/temporaryname/bastienfalcou/testnavigation/APIClient.kt
@@ -9,17 +9,20 @@ object APIClient {
     init {
         FuelManager.instance.basePath = "https://api.flickr.com/services/rest"
         FuelManager.instance.addRequestInterceptor(cUrlLoggingRequestInterceptor())
+        FuelManager.instance.timeoutInMillisecond = 60000
+        FuelManager.instance.baseParams = listOf(
+            "api_key" to "946f7b3931c2d9b16795a35515da4c8b",
+            "extras" to "url_sq",
+            "format" to "json",
+            "nojsoncallback" to 1
+        )
     }
 
     fun fetchMovies(completion: (String) -> Unit) {
         val bodyJson = """
         {
             "method": "flickr.photos.search",
-            "api_key": "946f7b3931c2d9b16795a35515da4c8b",
             "text": "horror",
-            "extras": "url_sq",
-            "format": "json",
-            "nojsoncallback": 1
         }
         """
 
