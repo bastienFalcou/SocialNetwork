cf81918feb88fab4cef7a45580a5d727ae2048dd
Bastien Falcou <bastien@fueled.com>
2018-12-23 17:34:24 +0100

    feat(APIClient): addition test api call

--- app/src/main/java/com/temporaryname/bastienfalcou/testnavigation/APIClient.kt
+++ app/src/main/java/com/temporaryname/bastienfalcou/testnavigation/APIClient.kt
@@ -1,7 +1,27 @@
 package com.temporaryname.bastienfalcou.testnavigation
 
+import com.github.kittinunf.fuel.httpGet
+import com.github.kittinunf.result.Result
+
 object APIClient {
     init {
         println("Some initialization")
+    }
+
+    fun fetchMovies(completion: (String) -> Unit) {
+        "https://api.flickr.com/services/rest/flickr.photos.search"
+            .httpGet()
+            .responseString { request, response, result ->
+                when (result) {
+                    is Result.Failure -> {
+                        val ex = result.getException()
+                        print(ex)
+                    }
+                    is Result.Success -> {
+                        val data = result.get()
+                        print(data)
+                    }
+                }
+        }
     }
 }
