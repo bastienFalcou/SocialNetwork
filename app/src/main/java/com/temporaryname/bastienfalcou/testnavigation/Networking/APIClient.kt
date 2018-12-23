39aef91bf20ea349b3c94f7d7c44449c138b838a
Bastien Falcou <bastien@fueled.com>
2018-12-24 00:45:22 +0100

    fix(APIClient): fix headers and parameters

--- app/src/main/java/com/temporaryname/bastienfalcou/testnavigation/Networking/APIClient.kt
+++ app/src/main/java/com/temporaryname/bastienfalcou/testnavigation/Networking/APIClient.kt
@@ -15,37 +15,33 @@ object APIClient {
         FuelManager.instance.basePath = "https://api.flickr.com/services/rest"
         FuelManager.instance.baseHeaders = mapOf(
             "Accept" to "application/json",
-            "Content-Type" to "application/json"
+            "Content-Type" to "application/json",
+            "Accept-Encoding" to "identity"
+        )
+        FuelManager.instance.baseParams = listOf(
+            "method" to "flickr.photos.search",
+            "text" to "horror",
+            "api_key" to API_KEY,
+            "extras" to EXTRAS,
+            "format" to FORMAT,
+            "nojsoncallback" to 1
         )
         FuelManager.instance.addRequestInterceptor(cUrlLoggingRequestInterceptor())
         FuelManager.instance.timeoutInMillisecond = TIMOUT_MILLISECONDS
     }
 
     fun fetchMovies(completion: (String) -> Unit) {
-        val bodyJson = """
-        {
-            "method": "flickr.photos.search",
-            "text": "horror",
-            "api_key": $API_KEY,
-            "extras": $EXTRAS,
-            "format": $FORMAT,
-            "nojsoncallback": 1
-        }
-        """
-
-        "".httpGet()
-            .body(bodyJson)
-            .responseString { request, response, result ->
-                when (result) {
-                    is Result.Failure -> {
-                        val ex = result.getException()
-                        println("Error API: " + ex)
-                    }
-                    is Result.Success -> {
-                        val data = result.get()
-                        completion(data)
-                    }
+        "/".httpGet().responseString { request, response, result ->
+            when (result) {
+                is Result.Failure -> {
+                    val ex = result.getException()
+                    println("Error API: " + ex)
                 }
+                is Result.Success -> {
+                    val data = result.get()
+                    completion(data)
+                }
+            }
         }
     }
 }
