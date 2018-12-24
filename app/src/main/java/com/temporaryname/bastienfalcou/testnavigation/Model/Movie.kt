f0118be5be90ae7e62891b184285bde18427c1be
Bastien Falcou <bastien@fueled.com>
2018-12-24 12:09:11 +0100

    feat(Movie): implement object mapping and deserialization

--- app/src/main/java/com/temporaryname/bastienfalcou/testnavigation/Model/Movie.kt
+++ app/src/main/java/com/temporaryname/bastienfalcou/testnavigation/Model/Movie.kt
@@ -1,7 +1,14 @@
 package com.temporaryname.bastienfalcou.testnavigation.Model
 
-class Movie(val id: String,
-            val owner: String,
-            val title: String,
-            val isPublic: Boolean,
-            var urlString: String) {}
+import com.github.kittinunf.fuel.core.ResponseDeserializable
+import com.google.gson.Gson
+
+data class Movie(val id: String,
+                 val owner: String,
+                 val title: String,
+                 val isPublic: Boolean,
+                 var urlString: String) {
+    class Deserializer: ResponseDeserializable<Array<Movie>> {
+        override fun deserialize(content: String): Array<Movie>? = Gson().fromJson(content, Array<Movie>::class.java)
+    }
+}
