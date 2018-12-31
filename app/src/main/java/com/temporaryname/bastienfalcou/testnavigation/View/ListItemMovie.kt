53a4666cb5ef83a81d4b3e865b047ef1906dd063
Bastien Falcou <bastien@fueled.com>
2018-12-30 19:46:25 -0500

    refactor(ListItemMovie): rename object throughout codebase to conform with our naming conventions

--- app/src/main/java/com/temporaryname/bastienfalcou/testnavigation/View/ListItemMovie.kt
+++ app/src/main/java/com/temporaryname/bastienfalcou/testnavigation/View/ListItemMovie.kt
@@ -3,6 +3,6 @@ package com.temporaryname.bastienfalcou.testnavigation.View
 import android.content.Context
 import android.view.View
 
-class list_item_movie: View {
+class ListItemMovie: View {
     constructor(context: Context): super(context)
 }
